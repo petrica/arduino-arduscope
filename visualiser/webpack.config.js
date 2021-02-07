39924859b8db2f03f8dd31302582afc91e77ecbf
Petrica Martinescu <petrica.martinescu@gmail.com>
2021-02-07 02:07:50 +0200

    Rename project

--- visualiser/webpack.config.js
+++ visualiser/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-        title: 'Output Management'
+        title: 'ArduScope - Simple oscilloscope'
     })
   ],
   output: {
